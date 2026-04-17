23cc3ecaed472a0a300571048d9b660de0606650
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-17 13:45:27 +0200

    feat: Digit recognition [vibe-code]

---
+++ utils.js
@@ -0,0 +1,6 @@
+function calculatePointerCoordinates(event) {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    return { x, y };
+}
